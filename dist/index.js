4a4fdea8ba3d6f50b49e609fc41cf4e2e6d2bd29
김보미 <kimbomi2172@naver.com>
2024-10-11 23:59:39 +0900

    ✨ feat: 고양이 리스트 띄우기

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log('Hello, Random Cats!');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const catList = document.getElementById('catList');
+// GET
+const getCats = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield fetch('https://api.thecatapi.com/v1/images/search?limit=9');
+        const cats = yield response.json();
+        return cats;
+    }
+    catch (error) {
+        console.log('고양이 목록 가져오기 실패', error);
+    }
+});
+// 고양이 카드 띄우기
+const createCatCard = () => __awaiter(void 0, void 0, void 0, function* () {
+    const cats = yield getCats();
+    cats.slice(0, 9).forEach((cat) => {
+        const catItem = document.createElement('div');
+        const catImage = document.createElement('img');
+        catItem.className =
+            'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-black hover:scale-105 transition-all';
+        catImage.src = cat.url;
+        catImage.alt = '고양이 사진';
+        catImage.className = 'w-full rounded-t-md h-20 md:h-40 object-cover';
+        catItem.appendChild(catImage);
+        catList === null || catList === void 0 ? void 0 : catList.appendChild(catItem);
+    });
+});
+createCatCard();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, Random Cats!')
+const catList = document.getElementById('catList') as HTMLElement
+
+// GET
+const getCats = async () => {
+  try {
+    const response = await fetch(
+      'https://api.thecatapi.com/v1/images/search?limit=9',
+    )
+    const cats = await response.json()
+    return cats
+  } catch (error) {
+    console.log('고양이 목록 가져오기 실패', error)
+  }
+}
+
+// 고양이 카드 띄우기
+const createCatCard = async () => {
+  const cats = await getCats()
+  cats.slice(0, 9).forEach((cat: { url: string }) => {
+    const catItem = document.createElement('div')
+    const catImage = document.createElement('img')
+
+    catItem.className =
+      'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-black hover:scale-105 transition-all'
+    catImage.src = cat.url
+    catImage.alt = '고양이 사진'
+    catImage.className = 'w-full rounded-t-md h-20 md:h-40 object-cover'
+    catItem.appendChild(catImage)
+    catList?.appendChild(catItem)
+  })
+}
+
+createCatCard()
