6d13f52c3cd35571c456ea7b2710ae68fdba7911
김보미 <kimbomi2172@naver.com>
2024-10-12 00:16:21 +0900

    ✨ feat: 페이지네이션

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const catList = document.getElementById('catList');
+const pagination = document.getElementById('pagination');
+const currentPage = 1;
+const itemsPerPage = 9;
 // GET
-const getCats = () => __awaiter(void 0, void 0, void 0, function* () {
+const getCats = (page) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const response = yield fetch('https://api.thecatapi.com/v1/images/search?limit=9');
+        const response = yield fetch(`https://api.thecatapi.com/v1/images/search?limit=${itemsPerPage}&page=${page}`);
         const cats = yield response.json();
         return cats;
     }
@@ -21,18 +24,40 @@ const getCats = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 // 고양이 카드 띄우기
-const createCatCard = () => __awaiter(void 0, void 0, void 0, function* () {
-    const cats = yield getCats();
+const createCatCard = (page) => __awaiter(void 0, void 0, void 0, function* () {
+    const cats = yield getCats(page);
+    catList.innerHTML = ''; // 이전 카드 지우기
     cats.slice(0, 9).forEach((cat) => {
         const catItem = document.createElement('div');
         const catImage = document.createElement('img');
         catItem.className =
-            'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-black hover:scale-105 transition-all';
+            'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-gray-300 hover:scale-105 transition-all';
         catImage.src = cat.url;
         catImage.alt = '고양이 사진';
         catImage.className = 'w-full rounded-t-md h-20 md:h-40 object-cover';
         catItem.appendChild(catImage);
         catList === null || catList === void 0 ? void 0 : catList.appendChild(catItem);
     });
+    updatePagination(page);
 });
-createCatCard();
+// 페이지네이션
+const updatePagination = (currentPage) => {
+    const totalPage = 5;
+    pagination.innerHTML = '';
+    for (let i = 1; i <= totalPage; i++) {
+        const button = document.createElement('button');
+        button.innerText = `${i}`;
+        button.className = `mx-1 px-4 py-2 border border-gray-300 ${i === currentPage
+            ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+            : 'text-gray-300 hover:bg-blue-500 transition-all hover:text-white'} rounded`;
+        button.disabled = i === currentPage; // 현재 페이지 버튼 비활성화
+        button.addEventListener('click', () => {
+            if (button.disabled)
+                return;
+            currentPage = i;
+            createCatCard(currentPage);
+        });
+        pagination.appendChild(button);
+    }
+};
+createCatCard(currentPage);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 const catList = document.getElementById('catList') as HTMLElement
+const pagination = document.getElementById('pagination') as HTMLElement
+
+const currentPage = 1
+const itemsPerPage = 9
 
 // GET
-const getCats = async () => {
+const getCats = async (page: number) => {
   try {
     const response = await fetch(
-      'https://api.thecatapi.com/v1/images/search?limit=9',
+      `https://api.thecatapi.com/v1/images/search?limit=${itemsPerPage}&page=${page}`,
     )
+
     const cats = await response.json()
     return cats
   } catch (error) {
@@ -14,20 +19,47 @@ const getCats = async () => {
 }
 
 // 고양이 카드 띄우기
-const createCatCard = async () => {
-  const cats = await getCats()
+const createCatCard = async (page: number) => {
+  const cats = await getCats(page)
+  catList.innerHTML = '' // 이전 카드 지우기
+
   cats.slice(0, 9).forEach((cat: { url: string }) => {
     const catItem = document.createElement('div')
     const catImage = document.createElement('img')
 
     catItem.className =
-      'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-black hover:scale-105 transition-all'
+      'cursor-pointer rounded-md flex flex-col p-1 items-center w-30 h-30 md:w-60 md:h-60 border border-gray-300 hover:scale-105 transition-all'
     catImage.src = cat.url
     catImage.alt = '고양이 사진'
     catImage.className = 'w-full rounded-t-md h-20 md:h-40 object-cover'
     catItem.appendChild(catImage)
     catList?.appendChild(catItem)
   })
+  updatePagination(page)
 }
 
-createCatCard()
+// 페이지네이션
+const updatePagination = (currentPage: number) => {
+  const totalPage = 5
+  pagination.innerHTML = ''
+
+  for (let i = 1; i <= totalPage; i++) {
+    const button = document.createElement('button')
+    button.innerText = `${i}`
+    button.className = `mx-1 px-4 py-2 border border-gray-300 ${
+      i === currentPage
+        ? 'bg-gray-300 text-gray-500 cursor-not-allowed'
+        : 'text-gray-300 hover:bg-blue-500 transition-all hover:text-white'
+    } rounded`
+    button.disabled = i === currentPage // 현재 페이지 버튼 비활성화
+
+    button.addEventListener('click', () => {
+      if (button.disabled) return
+      currentPage = i
+      createCatCard(currentPage)
+    })
+    pagination.appendChild(button)
+  }
+}
+
+createCatCard(currentPage)
